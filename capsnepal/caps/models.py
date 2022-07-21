a95f3f00570d41a6ae149ad9f32edff13973aca2
Inish-Bashyal <inish.bashyal99@gmail.com>
2022-07-21 07:59:06 +0545

    Backend work

--- capsnepal/caps/models.py
+++ capsnepal/caps/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Cap(models.Model):
     name = models.TextField(max_length=50, default='')
-    image = models.ImageField(upload_to='caps')
+    image = models.ImageField(upload_to='savedImages')
     desc = models.TextField(max_length=500)
     price = models.IntegerField()
     qty_sold = models.IntegerField(default=0)
--- capsnepal/capsnepal/settings.py
+++ capsnepal/capsnepal/settings.py
@@ -130,5 +130,5 @@ STATIC_URL = 'caps/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MEDIA_ROOT = os.path.join(BASE_DIR,'caps')
+MEDIA_ROOT = os.path.join(BASE_DIR,'caps/savedImages')
 MEDIA_URL = '/'
