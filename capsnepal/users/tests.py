4e7144e9d6ddfad935aaa25dcda4537b45227c34
Inish-Bashyal <inish.bashyal99@gmail.com>
2022-07-25 17:38:15 +0545

    Testing completed

--- capsnepal/users/tests.py
+++ capsnepal/users/tests.py
@@ -1,7 +1,7 @@
 import imp
 from django.test import SimpleTestCase
 from django.urls import reverse,resolve
-from users.views import register_name,login_user,logout,update_password,update_user,show_user_details,delete_user
+from users.views import register_name,login_user,logout,show_user_details
 
 # Create your tests here.
 class Testurls(SimpleTestCase):
@@ -19,18 +19,7 @@ class Testurls(SimpleTestCase):
         self.assertEquals(resolve(urls).func,logout)
         
         
-    # def test_case_delete_user_urls(self):
-    #     urls = reverse('delete_user')
-    #     self.assertEquals(resolve(urls).func,delete_user)
-        
     def test_case_show_user_details_urls(self):
         urls = reverse('show_user_details')
         self.assertEquals(resolve(urls).func,show_user_details)
         
-    def test_case_update_user_urls(self):
-        urls = reverse('update_user/<id>')
-        self.assertEquals(resolve(urls).func,update_user/id)
-        
-    # def test_case_update_password_urls(self):
-    #     urls = reverse('update_password')
-    #     self.assertEquals(resolve(urls).func,update_password)
